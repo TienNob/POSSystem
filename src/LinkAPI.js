40acbd53cf49e5e714175160428e04dbe0f8ec8f
TienNob <nhuttien8377@gmail.com>
2024-03-02 15:07:59 +0700

    change some things

--- src/LinkAPI.js
+++ src/LinkAPI.js
@@ -1,3 +1,2 @@
-export const LinkAPI = "http://localhost:8080/api/";
+export const LinkAPI = "http://localhost:8082/api/";
 // export const LinkAPI = "http://10.13.130.225:8080/api/";
-
--- src/component/total/Invoice.js
+++ src/component/total/Invoice.js
@@ -1,13 +1,49 @@
 import React from "react";
-import '../Oder.css'
+import "../Oder.css";
+import axios from "axios";
+import { LinkAPI } from "../../LinkAPI";
 
 function Invoice({ totalPrice, products, tableID }) {
+  const sendInvoiceDataToServer = () => {
+    const invoiceData = {
+      ban: { id: tableID },
+      orderDate: new Date(),
+      totalAmount: totalPrice,
+    };
+    console.log(invoiceData);
+    axios
+      .post(`${LinkAPI}orders`, invoiceData)
+      .then((response) => {
+        console.log("Invoice data sent successfully:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error sending invoice data:", error);
+      });
+  };
+  const sendOderItemTosSever = () => {
+    products.forEach((product) => {
+      const orderItem = {
+        order: { id: tableID },
+        product: product,
+        quantity: product.quantity, // Số lượng của mặt hàng
+      };
+      console.log(orderItem);
+      // axios
+      //   .post("http://192.168.1.7:8080/api/orderItems", orderItem)
+      //   .then(response => {
+      //     console.log("Order item sent successfully:", response.data);
+      //   })
+      //   .catch(error => {
+      //     console.error("Error sending order item:", error);
+      //   });
+    });
+  };
   return (
     <div className="invoice blackColor">
       <p className="blackColor text-center">{new Date().toLocaleString()}</p>
       <p className="blackColor">Số bàn: {tableID}</p>
       <table className="table table-bordered">
-        <thead >
+        <thead>
           <tr>
             <th>Sản phẩm</th>
             <th>Số lượng</th>
@@ -28,9 +64,10 @@ function Invoice({ totalPrice, products, tableID }) {
       </table>
       <hr className="mt-4"></hr>
       <div className="d-flex justify-content-between blackColor sumTotal">
-      <h4>Tổng cộng: </h4>
-      <h4>{totalPrice} K</h4>
+        <h4>Tổng cộng: </h4>
+        <h4>{totalPrice} K</h4>
       </div>
+      <button onClick={sendInvoiceDataToServer}>Xác nhận đặt hàng</button>
     </div>
   );
 }
