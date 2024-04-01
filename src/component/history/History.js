6b3d9a429306ab120bb59794361709acd6c25028
TienNob <nhuttien8377@gmail.com>
2024-04-01 21:29:41 +0700

    update Pagination

--- src/component/history/History.js
+++ src/component/history/History.js
@@ -3,6 +3,10 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import { Container, Table } from "react-bootstrap";
 import { LinkAPI } from "../../LinkAPI";
+import usePagination from "@mui/material/usePagination";
+
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 import "./history.css";
 
 function History() {
@@ -19,42 +23,30 @@ function History() {
     }
   };
 
-  const handleNextPage = () => {
-    setCurrentPage(currentPage + 1);
-  };
-
-  const handlePreviousPage = () => {
-    setCurrentPage(currentPage - 1);
-  };
-
-  const handleFirstPage = () => {
-    setCurrentPage(0);
-  };
-
-  const handleLastPage = () => {
-    setCurrentPage(totalPages - 1);
-  };
-
   useEffect(() => {
     axios
-        .get(`${LinkAPI}orders?page=${currentPage}&size=${pageSize}`)
-        .then((res) => {
-          const sortedHistory = res.data.content.sort((a, b) => {
-            return new Date(b.orderDate) - new Date(a.orderDate);
-          });
-          setHistory(sortedHistory);
-          setTotalPages(res.data.totalPages);
-          console.log(sortedHistory);
-        })
-        .catch((error) => {
-          console.error("Error fetching product data:", error);
+      .get(`${LinkAPI}orders?page=${currentPage}&size=${pageSize}`)
+      .then((res) => {
+        const sortedHistory = res.data.content.sort((a, b) => {
+          return new Date(b.orderDate) - new Date(a.orderDate);
         });
+        setHistory(sortedHistory);
+        setTotalPages(res.data.totalPages);
+
+        console.log(sortedHistory);
+      })
+      .catch((error) => {
+        console.error("Error fetching product data:", error);
+      });
   }, [currentPage, pageSize]);
+  const handlePageChange = (event, newPage) => {
+    setCurrentPage(newPage - 1);
+  };
 
   return (
-      <Container>
-        <Table className="history">
-          <thead>
+    <Container>
+      <Table className="history">
+        <thead>
           <tr>
             <th className="setHistoryColor">#</th>
             <th className="setHistoryColor">Số điện thoại</th>
@@ -62,47 +54,46 @@ function History() {
             <th className="setHistoryColor">Tổng thành tiền</th>
             <th className="setHistoryColor">Chi tiết hoá đơn</th>
           </tr>
-          </thead>
-          <tbody>
+        </thead>
+        <tbody>
           {history.map((item, index) => (
-              <tr key={item.id}>
-                <td className="setHistoryColor">{currentPage * pageSize + index + 1}</td>
-                <td className="setHistoryColor">
-                  {item.phoneNumber !== "" ? item.phoneNumber : "Khách lẻ"}
-                </td>
-                <td className="setHistoryColor">
-                  {new Date(item.orderDate)
-                      .toISOString()
-                      .slice(0, 19)
-                      .replace("T", " ")
-                      .replace(" ", ", ")}
-                </td>
-                <td className="setHistoryColor">{item.totalAmount} K</td>
-                <td className="setHistoryColor">
-                  <Link onClick={() => handleClick(item.id)} to="/orderdetail">
-                    Xem thêm
-                  </Link>
-                </td>
-              </tr>
+            <tr key={item.id}>
+              <td className="setHistoryColor">
+                {currentPage * pageSize + index + 1}
+              </td>
+              <td className="setHistoryColor">
+                {item.phoneNumber !== "" ? item.phoneNumber : "Khách lẻ"}
+              </td>
+              <td className="setHistoryColor">
+                {new Date(item.orderDate)
+                  .toISOString()
+                  .slice(0, 19)
+                  .replace("T", " ")
+                  .replace(" ", ", ")}
+              </td>
+              <td className="setHistoryColor">{item.totalAmount} K</td>
+              <td className="setHistoryColor">
+                <Link onClick={() => handleClick(item.id)} to="/orderdetail">
+                  Xem thêm
+                </Link>
+              </td>
+            </tr>
           ))}
-          </tbody>
-        </Table>
-        <div className="button-container">
-          <button onClick={handleFirstPage} disabled={currentPage === 0}>
-            First
-          </button>
-          <button onClick={handlePreviousPage} disabled={currentPage === 0}>
-            Previous
-          </button>
-          <span>Page {currentPage + 1} of {totalPages}</span>
-          <button onClick={handleNextPage} disabled={currentPage === totalPages - 1}>
-            Next
-          </button>
-          <button onClick={handleLastPage} disabled={currentPage === totalPages - 1}>
-            Last
-          </button>
-        </div>
-      </Container>
+        </tbody>
+      </Table>
+      <div className="button-container mt-4">
+        <Stack spacing={2}>
+          <Pagination
+            showFirstButton
+            showLastButton
+            className="history-pagination"
+            count={totalPages}
+            color="primary"
+            onChange={handlePageChange} // Call handlePageChange function on page change
+          />
+        </Stack>
+      </div>
+    </Container>
   );
 }
 
