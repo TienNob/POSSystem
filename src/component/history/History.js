0162b40f0fc7d51e601dc377286caeeffe608b29
vinhnhp <vinhnhp@vnpt.vn>
2024-04-01 12:02:02 +0700

    Phan trang

--- src/component/history/History.js
+++ src/component/history/History.js
@@ -7,6 +7,9 @@ import "./history.css";
 
 function History() {
   const [history, setHistory] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
 
   const handleClick = (id) => {
     if (id !== null && id !== undefined) {
@@ -16,57 +19,73 @@ function History() {
     }
   };
 
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
   useEffect(() => {
     axios
-      .get(`${LinkAPI}orders`)
-      .then((res) => {
-        setHistory(res.data);
-        console.log(res.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching product data:", error);
-      });
-  }, []);
+        .get(`${LinkAPI}orders?page=${currentPage}&size=${pageSize}`)
+        .then((res) => {
+          setHistory(res.data.content);
+          setTotalPages(res.data.totalPages);
+          console.log(res.data.content);
+        })
+        .catch((error) => {
+          console.error("Error fetching product data:", error);
+        });
+  }, [currentPage, pageSize]);
 
   return (
-    <Container>
-      <Table className="history">
-        <thead>
+      <Container>
+        <Table className="history">
+          <thead>
           <tr>
             <th className="setHistoryColor">#</th>
             <th className="setHistoryColor">Số điện thoại</th>
             <th className="setHistoryColor">Ngày thanh toán</th>
             <th className="setHistoryColor">Tổng thành tiền</th>
             <th className="setHistoryColor">Chi tiết hoá đơn</th>
-            {/* Add more table headers if needed */}
           </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
           {history.map((item, index) => (
-            <tr key={item.id}>
-              <td className="setHistoryColor">{index + 1}</td>
-              <td className="setHistoryColor">
-                {item.phoneNumber !== "" ? item.phoneNumber : "Khách lẻ"}
-              </td>
-
-              <td className="setHistoryColor">
-                {new Date(item.orderDate)
-                  .toISOString()
-                  .slice(0, 19)
-                  .replace("T", " ")
-                  .replace(" ", ", ")}
-              </td>
-              <td className="setHistoryColor">{item.totalAmount} K</td>
-              <td className="setHistoryColor">
-                <Link onClick={() => handleClick(item.id)} to="/orderdetail">
-                  Xem thêm
-                </Link>
-              </td>
-            </tr>
+              <tr key={item.id}>
+                <td className="setHistoryColor">{currentPage * pageSize + index + 1}</td> {/* STT tính từ currentPage và pageSize */}
+                <td className="setHistoryColor">
+                  {item.phoneNumber !== "" ? item.phoneNumber : "Khách lẻ"}
+                </td>
+                <td className="setHistoryColor">
+                  {new Date(item.orderDate)
+                      .toISOString()
+                      .slice(0, 19)
+                      .replace("T", " ")
+                      .replace(" ", ", ")}
+                </td>
+                <td className="setHistoryColor">{item.totalAmount} K</td>
+                <td className="setHistoryColor">
+                  <Link onClick={() => handleClick(item.id)} to="/orderdetail">
+                    Xem thêm
+                  </Link>
+                </td>
+              </tr>
           ))}
-        </tbody>
-      </Table>
-    </Container>
+          </tbody>
+        </Table>
+        <div>
+          <button onClick={handlePreviousPage} disabled={currentPage === 0}>
+            Previous
+          </button>
+          <span>{currentPage + 1}</span>
+          <button onClick={handleNextPage} disabled={currentPage === totalPages - 1}>
+            Next
+          </button>
+        </div>
+      </Container>
   );
 }
 
